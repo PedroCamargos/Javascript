2c1e7c629db113f5e8759625e8512fdee75dd12f
Pedro Camargos <79293589+PedroCamargos@users.noreply.github.com>
2022-01-10 15:39:44 -0300

    Create Exercicio1.js

---
+++ Exercicio1.js
@@ -0,0 +1,11 @@
+//exercicio laços de repetição em mensagens na tela (aparecendo 1 por 1)
+var inicio = 2;
+	while(inicio <= 10){
+		if ((inicio % 2) == 0) {
+			alert("Numero: " + inicio);
+			inicio = inicio + 1;
+		}else{
+			inicio = inicio + 1;
+		}
+	}	
+
